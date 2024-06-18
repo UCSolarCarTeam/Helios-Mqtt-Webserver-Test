d7bda535e39ed2b94ca2569481a34dbe15f5b0ef
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-06-17 20:41:02 -0600

    add more stuff

--- SolarMQTTClient.ts
+++ SolarMQTTClient.ts
@@ -1,18 +1,10 @@
-import { type MqttClient, connect } from "mqtt";
+import { type MqttClient, connect, IClientOptions } from "mqtt";
 
-import { createLightweightApplicationLogger } from "@/utils/logger";
-
-type Options = {
+type Options = IClientOptions & {
   url: string;
-  clientId: string;
-  username: string;
-  password: string;
-  clean: boolean;
 };
 
 const clientId = "server";
-
-const logger = createLightweightApplicationLogger("serverMqttClient.ts");
 
 const options: Options = {
   url: `mqtt://localhost:${process.env.MQTT_PORT || 1883}`,
@@ -32,7 +24,7 @@ export class SolarMQTTClient {
 
   constructor(
     messageRecCb: (message: Buffer) => void,
-    latencyRecCb: (latency: number) => void,
+    latencyRecCb: (latency: number) => void
   ) {
     this.client = connect(options);
     this.messageRecCb = messageRecCb;
@@ -46,10 +38,10 @@ export class SolarMQTTClient {
   private initializeListeners(client: MqttClient) {
     client.on("connect", () => {
       // connect to broker
-      logger.info(`${clientId} connected to MQTT broker`);
+      console.log(`${clientId} connected to MQTT broker`);
       client.subscribe(testTopic, (error) => {
         if (!error) {
-          logger.info(`${clientId} subscribed to ${testTopic}`);
+          console.log(`${clientId} subscribed to ${testTopic}`);
           client.publish(testTopic, testMessage); // publish message
         } else {
           console.error(`${clientId} subscription error:`, error);
@@ -67,226 +59,7 @@ export class SolarMQTTClient {
     });
 
     client.on("error", (error) => {
-      logger.error(`${clientId} MQTT Client Error:`, error);
-    });
-  }
-}
-mport { type MqttClient, connect } from "mqtt";
-
-import { createLightweightApplicationLogger } from "@/utils/logger";
-
-type Options = {
-  url: string;
-  clientId: string;
-  username: string;
-  password: string;
-  clean: boolean;
-};
-
-const clientId = "server";
-
-const logger = createLightweightApplicationLogger("serverMqttClient.ts");
-
-const options: Options = {
-  url: `mqtt://localhost:${process.env.MQTT_PORT || 1883}`,
-  clientId,
-  username: "urMom",
-  password: "hasAedes",
-  clean: true,
-};
-
-const testTopic = "test topic";
-const testMessage = "test  message";
-
-export class SolarMQTTClient {
-  client: MqttClient;
-  messageRecCb: (message: Buffer) => void;
-  latencyRecCb: (latency: number) => void;
-
-  constructor(
-    messageRecCb: (message: Buffer) => void,
-    latencyRecCb: (latency: number) => void,
-  ) {
-    this.client = connect(options);
-    this.messageRecCb = messageRecCb;
-    this.latencyRecCb = latencyRecCb;
-    this.initializeListeners(this.client);
-  }
-
-  public calculatePing() {
-    this.client.publish("ping", "");
-  }
-  private initializeListeners(client: MqttClient) {
-    client.on("connect", () => {
-      // connect to broker
-      logger.info(`${clientId} connected to MQTT broker`);
-      client.subscribe(testTopic, (error) => {
-        if (!error) {
-          logger.info(`${clientId} subscribed to ${testTopic}`);
-          client.publish(testTopic, testMessage); // publish message
-        } else {
-          console.error(`${clientId} subscription error:`, error);
-        }
-      });
-    });
-
-    client.on("message", (topic: string, message: Buffer) => {
-      if (topic !== "PONG") {
-        this.messageRecCb(message);
-      } else if (topic === "PONG") {
-        const latency = Date.now() - parseInt(message.toString());
-        this.latencyRecCb(latency);
-      }
-    });
-
-    client.on("error", (error) => {
-      logger.error(`${clientId} MQTT Client Error:`, error);
-    });
-  }
-}
-mport { type MqttClient, connect } from "mqtt";
-
-import { createLightweightApplicationLogger } from "@/utils/logger";
-
-type Options = {
-  url: string;
-  clientId: string;
-  username: string;
-  password: string;
-  clean: boolean;
-};
-
-const clientId = "server";
-
-const logger = createLightweightApplicationLogger("serverMqttClient.ts");
-
-const options: Options = {
-  url: `mqtt://localhost:${process.env.MQTT_PORT || 1883}`,
-  clientId,
-  username: "urMom",
-  password: "hasAedes",
-  clean: true,
-};
-
-const testTopic = "test topic";
-const testMessage = "test  message";
-
-export class SolarMQTTClient {
-  client: MqttClient;
-  messageRecCb: (message: Buffer) => void;
-  latencyRecCb: (latency: number) => void;
-
-  constructor(
-    messageRecCb: (message: Buffer) => void,
-    latencyRecCb: (latency: number) => void,
-  ) {
-    this.client = connect(options);
-    this.messageRecCb = messageRecCb;
-    this.latencyRecCb = latencyRecCb;
-    this.initializeListeners(this.client);
-  }
-
-  public calculatePing() {
-    this.client.publish("ping", "");
-  }
-  private initializeListeners(client: MqttClient) {
-    client.on("connect", () => {
-      // connect to broker
-      logger.info(`${clientId} connected to MQTT broker`);
-      client.subscribe(testTopic, (error) => {
-        if (!error) {
-          logger.info(`${clientId} subscribed to ${testTopic}`);
-          client.publish(testTopic, testMessage); // publish message
-        } else {
-          console.error(`${clientId} subscription error:`, error);
-        }
-      });
-    });
-
-    client.on("message", (topic: string, message: Buffer) => {
-      if (topic !== "PONG") {
-        this.messageRecCb(message);
-      } else if (topic === "PONG") {
-        const latency = Date.now() - parseInt(message.toString());
-        this.latencyRecCb(latency);
-      }
-    });
-
-    client.on("error", (error) => {
-      logger.error(`${clientId} MQTT Client Error:`, error);
-    });
-  }
-}
-mport { type MqttClient, connect } from "mqtt";
-
-import { createLightweightApplicationLogger } from "@/utils/logger";
-
-type Options = {
-  url: string;
-  clientId: string;
-  username: string;
-  password: string;
-  clean: boolean;
-};
-
-const clientId = "server";
-
-const logger = createLightweightApplicationLogger("serverMqttClient.ts");
-
-const options: Options = {
-  url: `mqtt://localhost:${process.env.MQTT_PORT || 1883}`,
-  clientId,
-  username: "urMom",
-  password: "hasAedes",
-  clean: true,
-};
-
-const testTopic = "test topic";
-const testMessage = "test  message";
-
-export class SolarMQTTClient {
-  client: MqttClient;
-  messageRecCb: (message: Buffer) => void;
-  latencyRecCb: (latency: number) => void;
-
-  constructor(
-    messageRecCb: (message: Buffer) => void,
-    latencyRecCb: (latency: number) => void,
-  ) {
-    this.client = connect(options);
-    this.messageRecCb = messageRecCb;
-    this.latencyRecCb = latencyRecCb;
-    this.initializeListeners(this.client);
-  }
-
-  public calculatePing() {
-    this.client.publish("ping", "");
-  }
-  private initializeListeners(client: MqttClient) {
-    client.on("connect", () => {
-      // connect to broker
-      logger.info(`${clientId} connected to MQTT broker`);
-      client.subscribe(testTopic, (error) => {
-        if (!error) {
-          logger.info(`${clientId} subscribed to ${testTopic}`);
-          client.publish(testTopic, testMessage); // publish message
-        } else {
-          console.error(`${clientId} subscription error:`, error);
-        }
-      });
-    });
-
-    client.on("message", (topic: string, message: Buffer) => {
-      if (topic !== "PONG") {
-        this.messageRecCb(message);
-      } else if (topic === "PONG") {
-        const latency = Date.now() - parseInt(message.toString());
-        this.latencyRecCb(latency);
-      }
-    });
-
-    client.on("error", (error) => {
-      logger.error(`${clientId} MQTT Client Error:`, error);
+      console.error(`${clientId} MQTT Client Error:`, error);
     });
   }
 }
